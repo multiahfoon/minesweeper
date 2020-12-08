bbe6896e44c2e6397812aab5ffcbd43cad3c4bd7
multiahfoon <multiahfoon@gmail.com>
2020-12-08 13:22:28 +1300

    add win function for correct mark on mine or correct guess on non mines

--- minesweeper.js
+++ minesweeper.js
@@ -31,43 +31,60 @@ var board = {
     {
       row: 1, 
       col: 1,
-      isMine: true,
+      isMine: false,
       isMarked: false,
       hidden: true,
       surroundingMines: 0
     }
-  ]
+  ],
+  numberOfMines: 1
 };
-
 function startGame () {
-  // Don't remove this function call: it makes the game work!
+
+  // loop will check how many mines surround each cell
   for(let i = 0; i < board.cells.length; i++) {
+    // the current cell is given a value from the output of the following function
     board.cells[i].surroundingMines = countSurroundingMines(board.cells[i]);
   }
+
+
+  const displayBoard = document.getElementsByClassName('board')[0];
+  displayBoard.addEventListener('click', checkForWin);
+  displayBoard.addEventListener('contextmenu', checkForWin);
+  
   lib.initBoard()
 }
 
-// Define this function to look for a win condition:
-//
-// 1. Are all of the cells that are NOT mines visible?
-// 2. Are all of the mines marked?
+let rightGuess = [];
+let rightMark = [];
 function checkForWin () {
 
-  // You can use this function call to declare a winner (once you've
-  // detected that they've won, that is!)
-  //   lib.displayMessage('You win!')
+  for(let i = 0; i < board.cells.length; i++) {
+    
+    // check if left click was correct guess
+    if(board.cells[i].isMine == false && board.cells[i].hidden == false && !rightGuess.includes(board.cells[i])) {
+      rightGuess.push(board.cells[i]);
+    } 
+
+    // check if right click was correct guess
+    if(board.cells[i].isMine == true && board.cells[i].hidden == true && board.cells[i].isMarked == true ) {
+      rightMark.push(board.cells[i]);
+    }
+
+    // check if the user won by selecting all non mines or by marking the mines correctly
+    if( rightGuess.length == (board.cells.length - board.numberOfMines) || rightMark.length == board.numberOfMines ) {
+      lib.displayMessage('You win!')
+    }
+  }
+
 }
 
-// Define this function to count the number of mines around the cell
-// (there could be as many as 8). You don't have to get the surrounding
-// cells yourself! Just use `lib.getSurroundingCells`: 
-//
-//
-// It will return cell objects in an array. You should loop through 
-// them, counting the number of times `cell.isMine` is true.
+
 function countSurroundingMines (cell) {
   var surroundingCells = lib.getSurroundingCells(cell.row, cell.col);
   let count = 0;
+
+  // this loop counts how many mines are arround an individual cell
   for(let i = 0; i < surroundingCells.length; i++) {
     if(surroundingCells[i].isMine == true) {
       count++;
